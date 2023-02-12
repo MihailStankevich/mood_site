80875b85327ce979b2e1d26254e86d8cdeb3d359
MihailStankevich <87997316+MihailStankevich@users.noreply.github.com>
2023-02-12 16:34:37 +0300

    Update settings.py

--- booksite/settings.py
+++ booksite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd!8a#*v6o9z!(8@t-z3$_w3fe@=tepm1o6hqx!0om$mjx=9=*-'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
